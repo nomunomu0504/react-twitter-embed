33182ccb3a1bf8733b6cd10c55ba5e89a70b5e0e
h.nomura <>
2023-01-11 03:31:02 +0900

    add storybook config

---
+++ .storybook/theme.js
@@ -0,0 +1,8 @@
+import { create } from "@storybook/theming";
+
+export const theme = create({
+  base: "light",
+  brandTitle: "nomunomu0504/react-twitter-embed storybook",
+  brandUrl: "https://nomunomu0504.github.io/react-twitter-embed",
+  brandImage: "",
+});
